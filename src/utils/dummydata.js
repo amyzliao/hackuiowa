648950553ad79081e2fa8aa6257283f227bed022
amyzliao <amzhliao@gmail.com>
2023-09-23 11:11:11 -0500

    added dummy data

---
+++ src/utils/dummydata.js
@@ -0,0 +1,116 @@
+const fakegroups = [
+    {
+        id: 0,
+        name: 'ieee',
+        users: [1,2,4,6]
+    },
+    {
+        id: 1,
+        name: 'gamers',
+        users: [3,4,5]
+    },
+    {
+        id: 2,
+        name: 'hackuiowa',
+        users: [4,5,6,1]
+    },
+    {
+        id: 3,
+        name: 'sweaty cs majors',
+        users: [6,7,8,0]
+    }
+]
+
+const fakeusers = [
+    {
+        id: 0,
+        name: 'bob',
+        email: 'bob@gmail.com',
+        busy: [
+            ['0900', '1200'],
+            ['1300', '1500'],
+            ['1530', '1600'],
+            ['1800', '2000']
+        ]
+    },
+    {
+        id: 1,
+        name: 'joe',
+        email: 'joe@gmail.com',
+        busy: [
+            ['0900', '1400'],
+            ['1700', '1900'],
+        ]
+    },
+    {
+        id: 2,
+        name: 'minseo',
+        email: 'minseo@gmail.com',
+        busy: [
+            ['0700', '1200'],
+            ['1300', '1700'],
+            ['1900', '2100'],
+        ]
+    },
+    {
+        id: 3,
+        name: 'joyce',
+        email: 'joyce@gmail.com',
+        busy: [
+            ['0900', '0930'],
+            ['1445', '1515'],
+        ]
+    },
+    {
+        id: 4,
+        name: 'amy',
+        email: 'amy@gmail.com',
+        busy: [
+            ['0900', '1230'],
+            ['1300', '1430'],
+            ['1600', '1700'],
+            ['2100', '2130']
+        ]
+    },
+    {
+        id: 5,
+        name: 'juwon',
+        email: 'juwon@gmail.com',
+        busy: [
+            ['0700', '1000'],
+            ['1400', '1500'],
+            ['1530', '1600'],
+            ['1800', '2000']
+        ]
+    },
+    {
+        id: 6,
+        name: 'tim',
+        email: 'tim@gmail.com',
+        busy: [
+            ['1350', '1500'],
+            ['1800', '1900']
+        ]
+    },
+    {
+        id: 7,
+        name: 'blake',
+        email: 'bob@gmail.com',
+        busy: [
+            ['0900', '1200'],
+            ['1200', '1300'],
+            ['1300', '1430'],
+            ['1500', '1545'],
+            ['1800', '1830']
+        ]
+    },
+    {
+        id: 8,
+        name: 'steve',
+        email: 'steve@gmail.com',
+        busy: [
+            ['0845', '0915'],
+            ['1600', '2000']
+        ]
+    },
+]
