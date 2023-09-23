6c44918610a3ad379ee4c1e356c141bfef9fbebe
amyzliao <amzhliao@gmail.com>
2023-09-23 14:20:14 -0500

    example compoent

--- frontend/src/components/grouplist.js
+++ frontend/src/components/grouplist.js
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-import { fakegroups, fakeusers } from '../utils/dummydata'
-import GroupList from '../components/grouplist'
+import React from "react";
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>When To Schmeet</h1>
-      <div>
+const GroupList = () => {
+    return (
+        <div>
             <div className="collapse bg-base-200">
                 <input type="radio" name="my-accordion-1" checked="checked" />
                 <div className="collapse-title text-xl font-medium">
@@ -35,23 +31,7 @@ export default function Home() {
                 </div>
             </div>
         </div>
-      <GroupList/>
-      <div class='w-3/4 border-2 border-solid border-white'>
-        <h4>Groups:</h4>
-
-        {fakegroups.map((group) => (
-          <h6 key={group.id}>{group.id} {group.name} {group.users}</h6>
-        ))}
-      </div>
-
-      <div class='w-3/4 border-2 border-solid border-white'>
-        <h5>Users:</h5>
-
-        {fakeusers.map((user) => (
-          <h6 key={user.id}>{user.id} {user.name} {user.email}</h6>
-        ))}
-      </div>
-
-    </main>
-  )
+    )
 }
+
+export default GroupList
