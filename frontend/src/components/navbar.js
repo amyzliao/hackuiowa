1e85144256a76b60844f1511a7839a6730f2b434
jpark612 <juwonpark2025@u.northwestern.edu>
2023-09-24 12:29:02 -0500

    merge changes

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -3,15 +3,15 @@ import React from "react";
 
 const NavBar = () => {
   return (
-    <div className="navbar bg-base-200">
-      <div className="navbar-start pl-10">
-        <a className="normal-case text-xl mx-50px">whentoschmeet</a>
+    <div className="navbar bg-base-200 text-sm">
+      <div className="navbar-start pl-10 ">
+        <a className=" text-sm  ">whentoschmeet</a>
       </div>
 
-      <div className="navbar-end pr-10">
-      
-        <a className="btn mx-50px bg-accent hover:bg-accent-focus">Register/Login</a>
-        
+      <div className="navbar-end pr-10 py-2">
+        <a className="btn mx-50px bg-accent bg-green-500 hover:bg-accent-focus">
+          Register/Login
+        </a>
       </div>
     </div>
   );
